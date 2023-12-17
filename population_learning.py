9758272e6b592826f35974402fecc002a4a2cf4e
Logan <loganswan37@gmail.com>
2023-12-17 12:37:48 -0500

    basic algebraic SL learning

---
+++ population_learning.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+
+# alphabet = a,b
+# SL-2 grammar
+
+# probability any given word contains a given bigram is:
+# 2 / (2+n) where n is the number of bigrams in the grammar
+
+# words heard before convergence (diff n from example above)
+n = 40
+
+# initial population conditions
+p_all = 1
+p_3 = 0 # probability grammar is any specific set of 3
+p_2 = 0 # note that this means these probabilities will not sum to 1
+p_1 = 0
+p_none = 0
+
+# this should sum to 1 instead:
+# p_all + 4*p_3 + 6*p_2 + 4*p_1 + p_none
+
+all_over_time = [p_all]
+none_over_time = [p_none]
+time = [0]
+for i in range(40):
+    # probability some specific bigram is in a given word the listener hears
+    # all bigrams will be the same in this setup
+    p_bigram_produced = (p_all / 3) + (p_3 * 6 / 5) + (p_2 * 3 / 2) + (p_1 * 2 / 3)
+    
+    # 1- probability that this bigram is NOT produced n times
+    p_bigram_learned = 1 - (1-p_bigram_produced)**n
+
+    # probability of any specific set is the probability each element is learned
+    # times the probability each other element is not learned
+    p_all = p_bigram_learned**4
+    p_3 = (p_bigram_learned**3) * (1-p_bigram_learned)
+    p_2 = (p_bigram_learned**2) * (1-p_bigram_learned)**2
+    p_1 = p_bigram_learned * (1-p_bigram_learned)**3
+    p_none = (1-p_bigram_learned)**4
+
+    # check sum to 1
+    print("none:", p_none)
+    print("1:", p_1)
+    print("2:", p_2)
+    print("3:", p_3)
+    print("all:", p_all)
+    print("sum:", p_all + 4*p_3 + 6*p_2 + 4*p_1 + p_none)
+
+    all_over_time.append(p_all)
+    time.append(i+1)
+    none_over_time.append(p_none)
+
+plt.plot(time, all_over_time)
+
+plt.plot(time, none_over_time)
+plt.show()
+
+
